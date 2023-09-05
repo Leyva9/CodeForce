5bdbeeb94e43c514e639c09443858a87e513896d
Leyva9 <leyva0919@gmail.com>
2023-09-04 22:41:19 -0400

    #

---
+++ VanyaAndFence/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] ageAndHeight = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int[] heigths = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int totalWidth = 0;
+        for(int i = 0; i < heigths.Length; i++)
+        {   
+            if(heigths[i] > ageAndHeight[1])
+            {
+                totalWidth+=2;
+            }
+            else {
+                totalWidth++;
+            }
+        }
+        Console.WriteLine(totalWidth);
+    }
+}
