b07ee0982e03b18d6acd2a8595e217d79cdd8afa
Leyva9 <leyva0919@gmail.com>
2023-12-09 21:36:45 -0500

    Adding new solved problems

---
+++ BinaryNotation/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        string binaryNumb = "";
+
+        if(number == 0) {
+            Console.WriteLine("0");
+            return;
+        }
+        else {
+            while(number > 0)
+            {
+                binaryNumb = (number%2).ToString() + binaryNumb;
+                number /= 2;
+            }
+        }
+        binaryNumb.Reverse();
+        Console.WriteLine(binaryNumb);
+    }
+}
